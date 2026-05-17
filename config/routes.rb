5137cd07bec1e71bb6d76f99dc88f738c241edfb
Ros Montis <huync0603@gmail.com>
2026-05-17 12:52:16 +0700

    Add destroy article function and various function layouts

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,8 @@ Rails.application.routes.draw do
   root "pages#home"
   get "about", to:"pages#about"
   # This line will give all the routes of 'articles'
-  resources :articles, only: [:show,:index,:new,:create,:edit,:update]
+
+  #At this point, we already have all the REST-ful routes for 'articles'
+  # resources :articles, only: [:show,:index,:new,:create,:edit,:update,:destroy]
+  resources :articles
 end
