404dc30fe428e09d9bab49c95cfe20693dabf56f
Ros Montis <huync0603@gmail.com>
2026-05-19 17:25:53 +0700

    Set up Front-End layouts

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,9 @@ require_relative "application"
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+#Use this to prevent Rails from inserting 'field_with_error' class
+#Above class will affect the layout of the page
+ActionView::Base.field_error_proc = Proc.new do |html_tag, instance| 
+    html_tag.html_safe
+end
