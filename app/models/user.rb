217638f1d26a4c0fda302dafa9d97bfa9801e88e
Ros Montis <huync0603@gmail.com>
2026-05-21 15:42:59 +0700

    Add user sign-up function

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,4 +9,6 @@ class User < ApplicationRecord
         length:{maximum: 105},
         format:{with: VALID_EMAIL_REGEX }
     has_secure_password
+    validates :password, length:{minimum:6}
+    validates :password_confirmation, presence: true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
   root "pages#home"
   get "about", to:"pages#about"
-  # This line will give all the routes of 'articles'
-
-  #At this point, we already have all the REST-ful routes for 'articles'
-  # resources :articles, only: [:show,:index,:new,:create,:edit,:update,:destroy]
-  resources :articles
+  resources :articles, :users
+  get 'sign-up', to: 'users#new'
 end
+
+
